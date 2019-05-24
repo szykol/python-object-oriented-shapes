6181d47cd177c37850b4321176be9b207e619864
szykol <szymonkolton@gmail.com>
2019-05-24 09:16:06 +0200

    Add Color enum class

--- Shapes.py
+++ Shapes.py
@@ -2,12 +2,13 @@ import descriptors as de
 import abc
 import numbers
 import math
+from colors import Color
 
 class ConvexPolygon(abc.ABC):
     @abc.abstractclassmethod
     def __init__(self):
-        self.fill_color = None
-        self.outline_color = None
+        self.fill_color = Color.BLACK
+        self.outline_color = Color.WHITE
 
     @abc.abstractclassmethod
     def area(self):
@@ -21,22 +22,25 @@ class ConvexPolygon(abc.ABC):
     def draw(self):
         pass
 
+
 class Triangle(ConvexPolygon):
     a = de.QuantityAndType(numbers.Real)
     b = de.QuantityAndType(numbers.Real)
     c = de.QuantityAndType(numbers.Real)
+
     def __init__(self, sides):
         self.a, self.b, self.c = sides
         super().__init__()
 
     def draw(self):
         pass
-    
+
     def perimeter(self):
         return self.a + self.b + self.c
 
     def area(self):
         return self.a * self.b / 2
+
 
 class ConvexQuadrilateral(ConvexPolygon):
     a = de.QuantityAndType(numbers.Real)
@@ -50,7 +54,7 @@ class ConvexQuadrilateral(ConvexPolygon):
 
     def draw(self):
         pass
-    
+
     def perimeter(self):
         return self.a + self.b + self.c + self.d
 
@@ -68,45 +72,54 @@ class RegularPolygon(ConvexPolygon):
 
     def draw(self):
         pass
-    
+
     def perimeter(self):
         return self.side * self.side_count
 
     def area(self):
         return self.side ** 2 * self.side_count / 4 * math.tan(math.radians(180 / self.side_count))
 
+
 class RegularHexagon(RegularPolygon):
     def __init__(self):
         return super().__init__(6)
+
 
 class RegularPentagon(RegularPolygon):
     def __init__(self):
         return super().__init__(5)
 
+
 class RegularOctagon(RegularPolygon):
     def __init__(self):
         return super().__init__(8)
+
 
 class IsoscelesTriangle(Triangle):
     def __init__(self, ramie, podst):
         return super().__init__((ramie, ramie, podst))
 
+
 class EquilateralTriangle(Triangle):
     def __init__(self, side):
         return super().__init__((side) * 3)
-    
+
+
 class Parallelogram(ConvexQuadrilateral):
     def __init__(self, a, b):
-        return super().__init__((a,b,a,b))
+        return super().__init__((a, b, a, b))
+
 
 class Kite(ConvexQuadrilateral):
     def __init__(self, a, b):
-        return super().__init__((a,a,b,b))
+        return super().__init__((a, a, b, b))
+
 
 class Rhombus(Parallelogram):
     def __init__(self, a):
         return super().__init__((a, a))
 
+
 class Square(Parallelogram):
     def __init__(self, a, b):
-        return super().__init__((a,a))
+        return super().__init__((a, a))
