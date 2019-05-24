072af653b310f209eee766a50a824a96e3fd9ea5
szykol <szymonkolton@gmail.com>
2019-05-24 13:00:03 +0200

    Fix colors

--- colors.py
+++ colors.py
@@ -1,12 +1,12 @@
 class Color:
-    RED = (255,0,0)
-    GREEN = (0, 255, 0)
-    BLUE = (0,0,255)
-    BLACK = (0,0,0)
-    WHITE = (255,255,255)
-    CYAN = (0,255,255)
-    YELLOW = (255,255,0)
-    MAGENTA = (255,0,255)
-    SILVER = (192,192,192)
-    GRAY = (128,128,128)
-    DARK_GREEN = (0,128,0)
+    RED = 'red'
+    GREEN = 'green'
+    BLUE = 'blue'
+    BLACK = 'black'
+    WHITE = 'white'
+    CYAN = 'cyan'
+    YELLOW = 'yellow'
+    MAGENTA = 'magenta'
+    SILVER = 'silver'
+    GRAY = 'gray'
+    DARK_GREEN = 'dark green'
