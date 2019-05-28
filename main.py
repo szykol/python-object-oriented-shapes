055e3291f10ad3f22d0acf91be009b3894037064
szykol <szymonkolton@gmail.com>
2019-05-28 09:21:10 +0200

    Update main

--- main.py
+++ main.py
@@ -1,13 +1,4 @@
 from Shapes import *
 
-# t = Triangle((10, 10, 10))
-# print(t.area())
-
-c = ConvexQuadrilateral(100, 100, 120, 0.5, 0.5)
-if (c.perimeter() == 4 * 5 * math.sqrt(2)):
-    print('correct perimeter')
-
-if (c.area() == 50):
-    print('correct area')
-
+c = ConvexQuadrilateral(100, 150, 0, 0.2, 0.8)
 c.draw()
