56ae193a128152dfd60e119f3323bab2a3bfb808
szykol <szymonkolton@gmail.com>
2019-05-30 18:35:12 +0200

    Add an option to choose which shape to draw

--- main.py
+++ main.py
@@ -1,4 +1,25 @@
+import Shapes
 from Shapes import *
 
-t = Square.fromInput()
-t.draw()
+import inspect
+
+possible_objects = []
+for name, obj in inspect.getmembers(Shapes):
+    if inspect.isclass(obj):
+        if obj.__module__ == 'Shapes' and obj.__name__ != 'ConvexPolygon':
+            possible_objects.append(obj)
+
+print('Dozwolone klasy: ')
+for index, value in enumerate(possible_objects):
+    print(index + 1, value.__name__)
+
+index = int(input('> '))
+
+chosen = possible_objects[index - 1].fromInput()
+chosen.draw()
+
+
+
+
+# t = Square.fromInput()
+# t.draw()
